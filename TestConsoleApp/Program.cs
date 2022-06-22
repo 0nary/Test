1f03a6bfb195d0379e621accaa7a3a0578b6c228
Poncelet Grégoire <102674985+0nary@users.noreply.github.com>
2022-06-22 11:15:43 +0200

    test(None): Nothing

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -6,7 +6,6 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello auto-versioning!");
             Console.ReadLine();
