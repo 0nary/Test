7bbb0ed71397a696c7064fa55c8401e706f5dc18
Poncelet Grégoire <102674985+0nary@users.noreply.github.com>
2022-09-02 15:47:23 +0200

    add test

---
+++ TestProject/Tests/Test_TestOrders.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Attributes;
+using Xunit;
+
+
+namespace TestProject.Tests
+{
+    [TestCaseOrderer("TestProject.Orderers.PriorityOrderer", "TestProject.Tests")]
+    public class Test_TestOrders
+    {
+        public static bool Test1Called = false;
+        public static bool Test2Called = false;
+        public static bool Test3Called = false;
+        public static bool Test4Called = false;
+        public static bool Test5Called = false;
+
+        [Fact, TestPriority(1)]
+        public void Test1()
+        {
+            Test1Called = true;
+
+            Assert.False(Test5Called, "5");
+            Assert.False(Test4Called, "4");
+            Assert.False(Test3Called, "3");
+            Assert.False(Test2Called, "2");
+            Assert.True(Test1Called, "1");
+        }
+
+        [Fact, TestPriority(3)]
+        public void Test3()
+        {
+            Test3Called = true;
+
+            Assert.False(Test5Called, "5");
+            Assert.False(Test4Called, "4");
+            Assert.True(Test3Called, "3");
+            Assert.True(Test2Called, "2");
+            Assert.True(Test1Called, "1");
+        }
+
+        [Fact, TestPriority(5)]
+        public void Test5()
+        {
+            Test5Called = true;
+
+            Assert.True(Test5Called, "5");
+            Assert.True(Test4Called, "4");
+            Assert.True(Test3Called, "3");
+            Assert.True(Test2Called, "2");
+            Assert.True(Test1Called, "1");
+        }
+
+        [Fact, TestPriority(4)]
+        public void Test4()
+        {
+            Test4Called = true;
+
+            Assert.False(Test5Called, "5");
+            Assert.True(Test4Called, "4");
+            Assert.True(Test3Called, "3");
+            Assert.True(Test2Called, "2");
+            Assert.True(Test1Called, "1");
+        }
+
+        [Fact, TestPriority(2)]
+        public void Test2()
+        {
+            Test2Called = true;
+
+            Assert.False(Test5Called, "5");
+            Assert.False(Test4Called, "4");
+            Assert.False(Test3Called, "3");
+            Assert.True(Test2Called, "2");
+            Assert.True(Test1Called, "1");
+        }
+    }
+}
