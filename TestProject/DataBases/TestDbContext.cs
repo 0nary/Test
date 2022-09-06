0aa53d9b4d438be993158e217e0547e04709dc55
Poncelet Grégoire <102674985+0nary@users.noreply.github.com>
2022-09-06 16:57:58 +0200

    fix(test) : revser commit

---
+++ TestProject/DataBases/TestDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.DataBases
+{
+    public class TestDbContext
+        :DbContext
+    {
+        private static DbContextOptionsBuilder<TestDbContext> _dbContextOptionsBuilder
+        {
+            get 
+            {
+                DbContextOptionsBuilder<TestDbContext> builder = new DbContextOptionsBuilder<TestDbContext>();
+                builder.UseSqlServer("Server=127.0.0.1,1433;Database=master;User Id=SA;Password=yourStrong(!)Password;MultipleActiveResultSets=true");
+                return builder;
+            }
+        }
+
+        public TestDbContext()
+            :base(_dbContextOptionsBuilder.Options)
+        {
+           
+        }
+    }
+}
---
+++ TestProject/Others/TestPriorityAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestProject.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestPriorityAttribute 
+        : Attribute
+    {
+        public TestPriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+
+        public int Priority { get; private set; }
+    }
+}
