3dc7859be35bf54f68a725f78eded0b1c9b4e34d
Poncelet Grégoire <102674985+0nary@users.noreply.github.com>
2022-09-29 13:28:43 +0200

    style(test): style1

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -8,6 +8,7 @@ namespace TestConsoleApp
         {
             //ci
             //test
+            //style
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello auto-versioning!");
             Console.WriteLine("Hello auto-versioning!");
